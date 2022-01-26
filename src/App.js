34926dc7a41ade41f86be801d2a3eb69cafabc9b
rafi <rafi.alfatih@gmail.com>
2022-01-26 20:52:42 +0700

    add sass

--- src/App.js
+++ src/App.js
@@ -12,20 +12,35 @@ const App = () => {
   }, [number, setState]);
 
   return (
-    <div>
-      <input
-        type="number"
-        name="numberTrivia"
-        onChange={(e) => setNumber(e.target.value)}
-        value={number}
-      />
-      {number === "" ? (
-        <p>Invalid data, please enter a number!</p>
-      ) : state.loading ? (
-        <p>loading...</p>
-      ) : (
-        <p>{state.data}</p>
-      )}
+    <div className="container">
+      <div className="content">
+        <h2>Trivia Number</h2>
+        <input
+          type="number"
+          name="numberTrivia"
+          onChange={(e) => setNumber(e.target.value)}
+          value={number}
+        />
+        <div className="result">
+          {number === "" ? (
+            <p>Invalid data, please enter a number!</p>
+          ) : state.loading ? (
+            <p>loading...</p>
+          ) : (
+            <p>{state.data}</p>
+          )}
+        </div>
+        <p className="about">
+          Trivia number is an app made by me for a learning purpose, it uses api
+          from{" "}
+          <a href="http://numbersapi.com" rel="noreferrer" target="_blank">
+            numbersapi.com.
+          </a>{" "}
+          Enter a number in the form, and you might find a fun fact about the
+          number! But remember! It has to be a round number, so any number other
+          than that is invalid!
+        </p>
+      </div>
     </div>
   );
 };
